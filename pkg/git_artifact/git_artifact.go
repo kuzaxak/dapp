d2ec685ce6128472b3008b4f482768e82c13763b
Timofey Kirillov <timofey.kirillov@flant.com>
2018-03-23 11:05:00 +0300

    ++

--- pkg/git_artifact/git_artifact.go
+++ pkg/git_artifact/git_artifact.go
@@ -1,11 +1,13 @@
 package git_artifact
 
 import (
+	"fmt"
 	"github.com/flant/dapp/pkg/git_repo"
 )
 
 type GitArtifact struct {
-	GitRepo git_repo.GitRepo
+	LocalGitRepo  *git_repo.LocalGitRepo
+	RemoteGitRepo *git_repo.RemoteGitRepo
 
 	Name               string
 	As                 string
@@ -20,6 +22,19 @@ type GitArtifact struct {
 	Paramshash         string
 }
 
+func (ga *GitArtifact) GitRepo() (git_repo.GitRepo, error) {
+	if ga.LocalGitRepo != nil {
+		return ga.LocalGitRepo, nil
+	} else if ga.RemoteGitRepo != nil {
+		return ga.RemoteGitRepo, nil
+	}
+	return nil, fmt.Errorf("GitRepo not initialized")
+}
+
 func (ga *GitArtifact) LatestCommit() (string, error) {
-	return ga.GitRepo.LatestCommit(ga.Branch)
+	gitRepo, err := ga.GitRepo()
+	if err != nil {
+		return "", err
+	}
+	return gitRepo.LatestCommit(ga.Branch)
 }
--- pkg/git_repo/git_repo.go
+++ pkg/git_repo/git_repo.go
@@ -1,8 +1,39 @@
 package git_repo
 
-type GitRepo struct {
+import (
+	"fmt"
+	"gopkg.in/src-d/go-git.v4"
+)
+
+type GitRepo interface {
+	LatestCommit(branch string) (string, error)
 }
 
-func (gr *GitRepo) LatestCommit(branch string) (string, error) {
-	return "deadbeef", nil
+type BaseGitRepo struct {
+	Name string
+}
+
+type LocalGitRepo struct {
+	BaseGitRepo
+	Path string
+}
+
+func (gr *LocalGitRepo) LatestCommit(branch string) (string, error) {
+	repository, err := git.PlainOpen(gr.Path)
+	if err != nil {
+		return "", err
+	}
+
+	_ = repository
+
+	return "", fmt.Errorf("not imlemented")
+}
+
+type RemoteGitRepo struct {
+	BaseGitRepo
+	Url string
+}
+
+func (gr *RemoteGitRepo) LatestCommit(branch string) (string, error) {
+	return "", fmt.Errorf("not imlemented")
 }
